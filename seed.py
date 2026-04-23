aa5abfa58ce8275933882105806a595cf1076f76
Samaro1 <asamaroingwu@gmail.com>
2026-04-23 14:09:46 +0100

    fix(database): indentation and scope of the db filling changed

--- seed.py
+++ seed.py
@@ -15,6 +15,8 @@ async def seed_database():
     print("=== SEED STARTED ===", flush=True)
     DATABASE_URL = os.getenv("DATABASE_URL")
 
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
     if not DATABASE_URL:
         raise RuntimeError("DATABASE_URL is not set")
 
@@ -24,48 +26,50 @@ async def seed_database():
     elif DATABASE_URL.startswith("postgresql+asyncpg://"):
         DATABASE_URL = DATABASE_URL.replace("postgresql+asyncpg://", "postgres://", 1)
 
-        await Tortoise.init(
+
+    await Tortoise.init(
         db_url=DATABASE_URL,
         modules={"models": ["models"]}
-        )
-        await Tortoise.generate_schemas()
+    )
 
-        print("Fetching seed data....")
-        async with httpx.AsyncClient(timeout=30) as client:
-            try:
-                response = await client.get(SEED_URL)
-                response.raise_for_status()
-                data = response.json()
-            except httpx.RequestError as e:
-                print(f"Failed to fetch seed data: {e}")
-                return
-            except httpx.HTTPStatusError as e:
-                print(f"HTTP error while fetching seed data: {e}")
-                return
-            except Exception as e:
-                print(f"Unexpected error: {e}")
-                return
-            profiles = data.get("profiles", [])
-            created= 0
-            skipped = 0
-            for profile in profiles:
-                exists = await Profile.filter(name=profile["name"]).first()
-                if exists:
-                    skipped += 1
-                    continue
-                await Profile.create(
-                    id=uuid7(),
-                    name=profile["name"],
-                    gender=profile["gender"],
-                    gender_probability=profile["gender_probability"],
-                    age=profile["age"],
-                    age_group=profile["age_group"],
-                    country_id=profile["country_id"],
-                    country_name=profile["country_name"],
-                    country_probability=profile["country_probability"],
-                )
-                created += 1
-            print(f"Seeding completed: {created} profiles created, {skipped} profiles skipped.")
+    await Tortoise.generate_schemas()
+
+    print("Fetching seed data....")
+    async with httpx.AsyncClient(timeout=30) as client:
+        try:
+            response = await client.get(SEED_URL)
+            response.raise_for_status()
+            data = response.json()
+        except httpx.RequestError as e:
+            print(f"Failed to fetch seed data: {e}")
+            return
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP error while fetching seed data: {e}")
+            return
+        except Exception as e:
+            print(f"Unexpected error: {e}")
+            return
+        profiles = data.get("profiles", [])
+        created= 0
+        skipped = 0
+        for profile in profiles:
+            exists = await Profile.filter(name=profile["name"]).first()
+            if exists:
+                skipped += 1
+                continue
+            await Profile.create(
+                id=uuid7(),
+                name=profile["name"],
+                gender=profile["gender"],
+                gender_probability=profile["gender_probability"],
+                age=profile["age"],
+                age_group=profile["age_group"],
+                country_id=profile["country_id"],
+                country_name=profile["country_name"],
+                country_probability=profile["country_probability"],
+            )
+            created += 1
+        print(f"Seeding completed: {created} profiles created, {skipped} profiles skipped.")
     await Tortoise.close_connections()
 
 
