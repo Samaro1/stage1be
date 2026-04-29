dd267832b568911b8718ca99f957c52805d322dc
Samaro1 <asamaroingwu@gmail.com>
2026-04-29 12:50:01 +0100

    feat(config): hardcoded the frontend url to debug

--- main.py
+++ main.py
@@ -52,6 +52,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5500",
         "http://127.0.0.1:5500",
+        "https://insighta-labs-web.vercel.app",
         FRONTEND_URL,
     ],
     allow_credentials=True,
