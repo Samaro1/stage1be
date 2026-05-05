fa1e8ebc0cb0285d5bd4880829dfce0f0b7bf627
Samaro1 <asamaroingwu@gmail.com>
2026-05-05 18:25:02 +0100

    feat(db): added an index table

--- models.py
+++ models.py
@@ -41,3 +41,20 @@ class RefreshToken(Model):
 
     class Meta:
         table = "refresh_tokens"
+
+class Meta:
+    table = "profiles"
+    indexes = (
+        ("gender",),
+        ("country_id",),
+        ("age_group",),
+
+        # range or sorting
+        ("age",),
+        ("gender_probability",),
+        ("country_probability",),
+        ("created_at",),
+
+        # composite
+        ("gender", "country_id"),
+    )
